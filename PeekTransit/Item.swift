5129a8a7111f49adf849387c715f9c9c633a5032
aymanhki <agamyahk@myumanitoba.ca>
2025-01-25 20:31:13 -0600

    Initial Commit

---
+++ PeekTransit/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  PeekTransit
+//
+//  Created by Ayman Agamy on 2025-01-25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
